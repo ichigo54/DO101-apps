f74afe374bdc07945cf5752d1b43e1e445e79c8c
ichigo54 <sathyabharath.reddy@gmail.com>
2020-05-23 17:46:43 +0000

    added /pig

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -4,6 +4,12 @@ app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
+app.get('/mars', function (req, res) {
+  res.send('Hello MArs-World!\n');
+});
+app.get('/pig', function (req, res) {
+  res.send('Hello Bhavya!\n');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
